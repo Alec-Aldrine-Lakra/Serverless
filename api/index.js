a2cbd73c91b753c6992b8d5d69b11ecc934e42df
Alec Aldrine Lakra <lakraalec123@gmail.com>
2019-12-14 23:54:24 +0530

    Serverless 0.4

--- api/index.js
+++ api/index.js
@@ -4,19 +4,20 @@ const uuid = require('uuid/v1');
 let message='';
 let url = 'mongodb+srv://melo_alec:AQrIy7ls9MhSOV6F@mflix-jx4le.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true }).then(() => {
-  message='Connected'
+mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true }).then(() => {
+  message = 'Connected';
+  console.log(message);
 }).catch(err => {
-  message = 'Could Not Connect'
+  message = 'Could Not Connect';
+  console.log(message);
 })
 
-module.exports = (req, res) => {
 
+module.exports = (req, res) => {
   res.json({
     body: req.body,
-    message: message,
+    uid: uuid(),
     query: req.query,
     cookies: req.cookies
   })
-  
 }
