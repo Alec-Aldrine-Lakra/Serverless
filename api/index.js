1bc5b3e189201e59fdb6483492bcefe1fe39bd38
Alec Aldrine Lakra <lakraalec123@gmail.com>
2019-12-14 23:33:01 +0530

    Serverless 0.3

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const uuid = require('uuid/v1');
 
-module.exports = (req, res) => {
+let message='';
+let url = 'mongodb+srv://melo_alec:AQrIy7ls9MhSOV6F@mflix-jx4le.mongodb.net/test?retryWrites=true&w=majority';
 
-  let message='';
-  let url = 'mongodb+srv://melo_alec:AQrIy7ls9MhSOV6F@mflix-jx4le.mongodb.net/test?retryWrites=true&w=majority';
-  
-  mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true }).then(() => {
-    message='Connected'
-  }).catch(err => {
-    message = 'Could Not Connect'
-  })
+mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true }).then(() => {
+  message='Connected'
+}).catch(err => {
+  message = 'Could Not Connect'
+})
+
+module.exports = (req, res) => {
 
   res.json({
     body: req.body,
